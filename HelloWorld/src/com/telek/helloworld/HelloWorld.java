1b705fd370acbb00373d7095f439a2e9e1cb6104
lichenxing <664940939@qq.com>
2016-09-28 23:49:02 +0800

    first Commit!

---
+++ HelloWorld/src/com/telek/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * ≤‚ ‘≥Ã–Ú
+ */
+package com.telek.helloworld;
+
+/**
+ * @author LiChenxing
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello github!");
+	}
+
+}
